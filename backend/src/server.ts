51a5278a9b786f319f0fab70b31b7fdd1dad8d6e
Saad Sayyed <saadsyed950@gmail.com>
2026-08-05 21:27:02 +0530

    Server setup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,11 @@
 import express from "express";
+import cors from "cors";
+import { env } from "./config/env.config";
 
 const app = express();
+const PORT = env.PORT;
 
-app.listen(8080, () => console.log(`Running`));
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`));
