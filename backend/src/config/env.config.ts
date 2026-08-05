b9329bb625ff456825723b3f085a1c337384b475
Saad Sayyed <saadsyed950@gmail.com>
2026-08-05 21:43:35 +0530

    Profile picture API handler

--- backend/src/config/env.config.ts
+++ backend/src/config/env.config.ts
@@ -3,4 +3,6 @@ import "dotenv/config";
 export const env = {
   PORT: process.env.PORT || 8000,
   JWT_SECRET: process.env.JWT_SECRET! || "sameoldtreva",
+  PFP_API:
+    process.env.PFP_API || "https://api.dicebear.com/10.x/notionists/svg?seed=",
 };
