b7bb4c21348e9109a60b76a90824832a1aed8d7c
Saad Sayyed <saadsyed950@gmail.com>
2026-08-12 18:37:06 +0530

    Register User: API

--- backend/src/lib/pfp.ts
+++ backend/src/lib/pfp.ts
@@ -1,5 +1,5 @@
 import { env } from "../config/env.config";
 
 export const profilePictureAPI = (seed: string) => {
-  return `${env.PFP_API}${seed}`;
+  return `${env.PFP_API}${seed.replace(" ", "_")}`;
 };
