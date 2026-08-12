9e3c8c83c4b1ce49c76b5b0f0d6ace5df04f8e2e
Saad Sayyed <saadsyed950@gmail.com>
2026-08-12 18:41:36 +0530

    Login User: route

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -4,5 +4,6 @@ import * as controllers from "../controllers/user.controller";
 const userRouter = Router();
 
 userRouter.post("/signup", controllers.registerUserController);
+userRouter.post("/login", controllers.loginUserController);
 
 export default userRouter;
