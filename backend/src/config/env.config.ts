b68710052b9a380e98393ab86588f3eae74fea24
Saad Sayyed <saadsyed950@gmail.com>
2026-08-12 14:42:28 +0530

    Working on frontend part 1st

--- backend/src/config/env.config.ts
+++ backend/src/config/env.config.ts
@@ -4,5 +4,6 @@ export const env = {
   PORT: process.env.PORT || 8000,
   JWT_SECRET: process.env.JWT_SECRET! || "sameoldtreva",
   PFP_API:
-    process.env.PFP_API || "https://api.dicebear.com/10.x/notionists/svg?seed=",
+    process.env.PFP_API! ||
+    "https://api.dicebear.com/10.x/notionists/svg?seed=",
 };
