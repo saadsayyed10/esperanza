c021bb464297da58636570b03117d5f28afcba13
Saad Sayyed <saadsyed950@gmail.com>
2026-08-12 18:26:28 +0530

    Main router middleware

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,12 +2,15 @@ import express from "express";
 import cors from "cors";
 import { env } from "./config/env.config";
 import { dbConnect } from "./config/db.config";
+import mainRouter from "./routes/index.route";
 
 const app = express();
 const PORT = env.PORT;
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api", mainRouter);
 
 const startServer = async () => {
   await dbConnect();
