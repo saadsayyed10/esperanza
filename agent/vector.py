23f5d1d73bd22c3b27c82a77af9361a7150f874a
Saad Sayyed <saadsyed950@gmail.com>
2026-08-18 11:04:24 +0530

    Vector handler

---
+++ agent/vector.py
@@ -0,0 +1,36 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+
+from models.embedding import embeddingModel
+
+def createRetriever(pdfPath: str):
+    # Load document
+    loader = PyPDFLoader(pdfPath)
+    documents = loader.load()
+
+    # Set dir location
+    dbLocation = "./chroma_db"
+
+    # Configure chunking of pdf document
+    textSplitter = RecursiveCharacterTextSplitter(
+        chunk_size=800,
+        chunk_overlap=1000
+    )
+
+    chunks = textSplitter.split_documents(documents)
+
+    # Vector database to store vector
+    vectorStore = Chroma(
+        collection_name="resume_chunks",
+        embedding_function=embeddingModel,
+        persist_directory=dbLocation
+    )
+
+    vectorStore.add_documents(chunks)
+
+    retriever = vectorStore.as_retriever(
+        search_kwargs={"k": 5}
+    )
+
+    return retriever
